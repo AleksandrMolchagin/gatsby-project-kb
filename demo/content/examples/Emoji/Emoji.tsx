aa260561f69ce6c076fda935a50c2bf75e75ee37
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2021-11-30 16:28:09 -0500

    Creat intro page

--- demo/content/examples/Emoji/Emoji.tsx
+++ demo/content/examples/Emoji/Emoji.tsx
@@ -3,19 +3,31 @@ import React from 'react'
 
 interface EmojiProps{
     name: String;
+    type: String
 }
 
 const Emoji: React.FC<EmojiProps> = (props) => {
-
-     
-    return (
-        <span>
-            <img 
-                src={require(`./emojis/${props.name}.png`).default} 
-                className="inline-emoji" 
-                alt={`Emoji: ${props.name}`}></img>
-        </span>
-    )
+    
+    switch(props.type){
+        case "h1":
+            return (
+                <span>
+                    <img 
+                        src={require(`./emojis/${props.name}.png`).default} 
+                        className="inline-emoji-h1" 
+                        alt={`Emoji: ${props.name}`}></img>
+                </span>
+            );
+        default:
+            return (
+                <span>
+                    <img 
+                        src={require(`./emojis/${props.name}.png`).default} 
+                        className="inline-emoji" 
+                        alt={`Emoji: ${props.name}`}></img>
+                </span>
+            )
+    }
 }
 export default Emoji
 
